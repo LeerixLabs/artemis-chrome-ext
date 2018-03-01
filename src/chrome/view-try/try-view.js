2180520449975f555cfa7f16e26f355aa9bff6b2
Uri Kalish <uri.kalish@hpe.com>
2018-03-01 11:42:04 +0200

    add find element button

--- src/chrome/view-try/try-view.js
+++ src/chrome/view-try/try-view.js
@@ -13,13 +13,35 @@ angular.module('mainApp').controller('tryCtrl', function tryCtrl($scope, commonF
 		uiStrings: {
 			titlePrimary: 'Try',
 			titleSecondary: 'Execute a single NLT command on the current active browser tab',
+			findButtonText: 'Find',
+			findButtonTooltip: 'Find element',
 			runButtonText: 'Run',
 			runButtonTooltip: 'Run command'
 		}
 	};
 
+	$scope.canFind = function canFind() {
+		return !!$scope.model.sentence.trim();
+	};
+
 	$scope.canRun = function canRun() {
 		return !!$scope.model.sentence.trim();
+	};
+
+	$scope.find = function run() {
+		commonFunctions.getActiveTab(function() {
+			chrome.runtime.sendMessage({
+				type: 'artemis-msg-execute-commands-extension-ui',
+				msg: JSON.stringify([
+					{
+						command: 'debug',
+						data: $scope.model.sentence.trim()
+					}
+				], null, 2),
+			}, function() {
+				//window.close();
+			});
+		});
 	};
 
 	$scope.run = function run() {
