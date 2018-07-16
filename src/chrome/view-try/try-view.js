30583f709628cb0f35b8aec7d9e92c4d567e2c3a
Uri Kalish <uri.kalish@hpe.com>
2018-07-16 14:03:25 +0300

    sentence change timer to find and enter key to run

--- src/chrome/view-try/try-view.js
+++ src/chrome/view-try/try-view.js
@@ -6,17 +6,38 @@ angular.module('mainApp').directive('tryView', function () {
 	};
 });
 
-angular.module('mainApp').controller('tryCtrl', function tryCtrl($scope, commonFunctions) {
+angular.module('mainApp').controller('tryCtrl', function tryCtrl($scope, $timeout, commonFunctions) {
+
+	let promise = null;
 
 	$scope.model = {
 		sentence: 'find link',
 		uiStrings: {
 			titlePrimary: 'Try',
 			titleSecondary: 'Execute a single Artemis command on the current active browser tab',
+			sentenceInputTooltip: 'Try an Artemis command',
 			findButtonText: 'Find',
 			findButtonTooltip: 'Find element',
 			runButtonText: 'Run',
 			runButtonTooltip: 'Run command'
+		}
+	};
+
+	$scope.onChanged = function() {
+		if (promise) {
+			$timeout.cancel(promise);
+		}
+		promise = $timeout(() => {
+			if ($scope.canFind()) {
+				$scope.find();
+			}
+		}, 1000);
+	};
+
+	$scope.onKeyPressed = function($event) {
+		let keyCode = $event.which || $event.keyCode;
+		if (keyCode === 13 && $scope.canRun()) {
+			$scope.run();
 		}
 	};
 
@@ -28,7 +49,7 @@ angular.module('mainApp').controller('tryCtrl', function tryCtrl($scope, commonF
 		return !!$scope.model.sentence.trim();
 	};
 
-	$scope.find = function run() {
+	$scope.find = function find() {
 		commonFunctions.getActiveTab(function() {
 			chrome.runtime.sendMessage({
 				type: 'artemis-msg-execute-commands-extension-ui',
